4e5d8026f256df4e585824f9f529011b35337033
Daniel Niemeyer <danieldn94@gmail.com>
2015-07-27 11:24:40 -0700

    Initial Commit

---
+++ Othello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Othello
+//
+//  Created by Daniel Niemeyer on 7/27/15.
+//  Copyright (c) 2015 Daniel Niemeyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Othello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Othello
+//
+//  Created by Daniel Niemeyer on 7/27/15.
+//  Copyright (c) 2015 Daniel Niemeyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
